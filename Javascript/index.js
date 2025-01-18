98d5043012f14a54bd82875aa05f2b2709621913
savanesheetal4 <savanesheetal4@gmail.com>
2025-01-18 15:41:44 +0530

    message

---
+++ Javascript/index.js
@@ -0,0 +1,11 @@
+//console.log("Welcome to PVPPCOE")
+//console.log("This is console statement")
+
+
+let a=20,b=10;
+if(a>b){
+    console.log("a is greater")
+}
+else{
+    console.log("b is greater")
+}
